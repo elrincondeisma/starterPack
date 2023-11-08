e7f399d431b869c55473450b8e7ce589762d4ce6
Ismael Catala <ismael.catala@smileandlearn.com>
2023-11-08 12:39:59 +0100

    feat:

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -10,6 +10,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps<
                 <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
                     <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none  md:text-5xl lg:text-6xl text-white">Laravel Starter Pack</h1>
                     <p className="mb-8 text-lg font-normal lg:text-xl sm:px-16 lg:px-48 text-gray-400">Free starter pack with Laravel + Breeze + React + Shadcn UI</p>
+                    <p className="mb-8 text-lg font-normal lg:text-xl sm:px-16 lg:px-48 text-gray-400">Designed by <a href='https://www.youtube.com/@elrincondeisma?sub_confirmation=1' target='_blank'>El Rincón de Isma</a></p>
                     
                 </div>
             </section>
